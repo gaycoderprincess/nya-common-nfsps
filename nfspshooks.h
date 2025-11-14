cd9d9efc8a02be286f2b805d6bcac807e5e02c4c
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-11-14 13:26:48 +0100

    input blocker

--- nfspshooks.h
+++ nfspshooks.h
@@ -4,6 +4,7 @@ namespace NyaHooks {
 	std::vector<void(*)()> aEndSceneFuncs;
 	std::vector<void(*)()> aWorldServiceFuncs;
 	std::vector<void(*)(HWND, UINT, WPARAM, LPARAM)> aWndProcFuncs;
+	bool bInputsBlocked = false;
 
 	auto EndSceneOrig = (uint32_t(__thiscall*)(void*))nullptr;
 	uint32_t __thiscall EndSceneHook(void* a1) {
@@ -19,6 +20,12 @@ namespace NyaHooks {
 			func();
 		}
 		return WorldServiceOrig();
+	}
+
+	auto InputBlockerOrig = (bool(__cdecl*)())nullptr;
+	bool InputBlockerHook() {
+		if (bInputsBlocked) return true;
+		return InputBlockerOrig();
 	}
 
 	auto WndProcOrig = (LRESULT(__stdcall*)(HWND, UINT, WPARAM, LPARAM))nullptr;
@@ -39,6 +46,13 @@ namespace NyaHooks {
 	void PlaceWorldServiceHook() {
 		if (!WorldServiceOrig) {
 			WorldServiceOrig = (void(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6DB068, &WorldServiceHook);
+		}
+	}
+
+	void PlaceInputBlockerHook() {
+		if (!InputBlockerOrig) {
+			// thanks to Xanvier for helping out with this one ^^
+			InputBlockerOrig = (bool(__cdecl*)())NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x6CA27D, &InputBlockerHook);
 		}
 	}
 
