ac295ad75459c13f7339ac3e19cd057f539da75f
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-11-15 16:21:16 +0100

    Attrib::Collection, Instance

--- types/Attrib.h
+++ types/Attrib.h
@@ -1,4 +1,33 @@
 namespace Attrib {
-	auto FindCollection = (uint32_t(__cdecl*)(uint32_t classKey, uint32_t collectionKey))0x52CD40;
+	class Vault;
+
+	class Collection {
+	public:
+		uint8_t _mTable[0xC];
+		Collection* mParent;
+		uint32_t mKey;
+		void* mClass;
+		void* mLayout;
+		Vault* mSource;
+	};
+
+	class Instance {
+	public:
+		Collection* mCollection;
+		void* mLayoutPtr;
+		uint32_t mMsgPort;
+		uint32_t mFlags;
+
+		Instance(const Collection* collection, uint32_t msgPort) {
+			((Instance*(__thiscall*)(Instance*, const Collection*, uint32_t))0x52AA00)(this, collection, msgPort);
+		}
+		~Instance() {
+			((void(__thiscall*)(Instance*))0x5301F0)(this);
+		}
+
+		static inline auto GetData = (void*(__thiscall*)(Instance*, uint32_t attributeKey, uint32_t index))0x52B950;
+	};
+
+	auto FindCollection = (Collection*(__cdecl*)(uint32_t classKey, uint32_t collectionKey))0x52CD40;
 	auto StringHash32 = (uint32_t(__cdecl*)(const char*))0x52B8D0;
 };
